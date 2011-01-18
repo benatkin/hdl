86a8730f6b1ca124e1cfb26f428e8d0e606b52e7
Ben Atkin <ben@benatkin.com>
2011-01-18 00:26:44 -0700

    built outline and converted to ul

--- msa.py
+++ msa.py
@@ -4,6 +4,7 @@ import lxml.etree
 import lxml.html
 import lxml.cssselect
 import re
+import json
 
 class WikiPage(object):
     def get_xml_url(self):
@@ -41,6 +42,9 @@ class WikiPage(object):
         utf8_parser = lxml.html.HTMLParser(encoding='utf-8')
         self.html_tree = lxml.html.fromstring(self.html, parser=utf8_parser)
 
+    def build(self):
+        self.get_data()
+
 class MSA(WikiPage):
     title = 'Table_of_United_States_Metropolitan_Statistical_Areas'
     count = 50
@@ -64,9 +68,6 @@ class MSA(WikiPage):
     def get_data(self):
         self.get_all_data()
         self.data = self.all_data[:self.count]
-                
-    def build(self):
-        self.get_data()
 
 class Regions(WikiPage):
     title = "List_of_regions_of_the_United_States"
@@ -90,16 +91,113 @@ class Regions(WikiPage):
                 region["divisions"].append(division)
             self.data.append(region)
 
+class Abbreviations(WikiPage):
+    title = "List_of_U.S._state_abbreviations"
+    state_column = 0
+    usps_column = 1
+    last_usps = 'WY'
+
+    def get_table(self):
+        self.get_html_tree()
+        table_select = lxml.cssselect.CSSSelector('table:contains("FIPS")')
+        self.table = table_select(self.html_tree)[0]
+
+    def get_data(self):
+        self.get_table()
+        self.data = {}
+        for abbr_tr in self.table.xpath('./tr'):
+            col_td = abbr_tr.xpath('./td')
+            if len(col_td) > self.usps_column:
+                abbr = {}
+                abbr['usps'] = col_td[self.usps_column].xpath('./text()')[0]
+                abbr['state'] = col_td[self.state_column].xpath('.//a/text()')[0]
+                self.data[abbr['usps']] = abbr['state']
+                if abbr['usps'] == self.last_usps:
+                    break
+
+class MSAOutline(object):
+    msa_re = re.compile(r'(\S+)\s*MSA\s*$')
+    region_for_state = {}
+
+    def get_msa(self):
+        msa = MSA()
+        msa.build()
+        self.msa = msa.data
+
+    def get_regions(self):
+        regions = Regions()
+        regions.build()
+        self.regions = regions.data
+
+    def get_abbreviations(self):
+        abbreviations = Abbreviations()
+        abbreviations.build()
+        self.abbreviations = abbreviations.data
+    
+    def add_states_to_msa(self):
+        for area in self.msa:
+            area["states"] = []
+            for abbr in self.msa_re.search(area["name"]).group(1).split("-"):
+                area["states"].append(self.abbreviations[abbr])
+
+    def build_region_for_state(self):
+        self.region_for_state = {}
+        for region in self.regions:
+            for division in region["divisions"]:
+                for state in division["states"]:
+                    self.region_for_state[state] = region["name"]
+
+    def build_outline(self):
+        outline = {}
+        for msa in self.msa:
+            state = msa["states"][0]
+            region = self.region_for_state[state]
+            if not region in outline:
+                outline[region] = {}
+            if not state in outline[region]:
+                outline[region][state] = []
+            outline[region][state].append("%s (%d)" % (msa["name"], msa["rank"]))
+            self.outline = outline
+        self.outline = [(region["name"], outline[region["name"]]) for region in self.regions]
+
+    def render_row(self, level, text):
+        return "    " * level + "*   " + text + "\n"
+
+    def render_outline(self):
+        self.markdown = self.render_row(0, "Metropolitain Statistical Areas")
+        for region in self.outline:
+            self.markdown += self.render_row(1, region[0])
+            states = region[1].keys()
+            states.sort()
+            for state in states:
+                self.markdown += self.render_row(2, state)
+                for msa in region[1][state]:
+                    self.markdown += self.render_row(3, msa)
+
+    def get_data(self):
+        self.get_msa()
+        self.get_regions()
+        self.get_abbreviations()
+        self.add_states_to_msa()
+        self.build_region_for_state()
+        self.build_outline()
+        self.render_outline()
+        self.data = {}
+        self.data["msa"] = self.msa
+        self.data["regions"] = self.regions
+        self.data["abbreviations"] = self.abbreviations
+        self.data["region_for_state"] = self.region_for_state
+        self.data["outline"] = self.outline
+
     def build(self):
         self.get_data()
 
-class Abbreviations(WikiPage):
-    pass
-
 if __name__ == "__main__":
-    msa = MSA()
-    msa.build()
-    print msa.data
-    regions = Regions()
-    regions.build()
-    print regions.data
+    print '-- data --'
+    msa_outline = MSAOutline()
+    msa_outline.build()
+    print '-- json --'
+    print json.dumps(msa_outline.data, indent=2, sort_keys=True)
+    print
+    print '-- markdown --'
+    print msa_outline.markdown
--- readme.markdown
+++ readme.markdown
@@ -13,13 +13,98 @@ today, usually under different names.
 
 ## City Example
 
-As an example, I'll use an unordered list, of the [20 largest metropolitain
-areas in the U. S., by
-state](http://en.wikipedia.org/wiki/Table_of_United_States_Metropolitan_Statistical_Areas).
+As an example, I'll use an unordered list, of the 50
+[largest metropolitain areas in the U. S., by state](http://en.wikipedia.org/wiki/Table_of_United_States_Metropolitan_Statistical_Areas).
 Some of these metro areas are spread across states; I'll use the state which
 has most of the metro's people living in it, which is conveniently listed
 first. To add an additional level, I've broken these into regions.
 
+*   Metropolitain Statistical Areas
+    *   Northeast
+        *   Connecticut
+            *   Hartford-West Hartford-East Hartford, CT MSA (45)
+        *   Massachusetts
+            *   Boston-Cambridge-Quincy, MA-NH MSA (10)
+        *   New York
+            *   New York-Northern New Jersey-Long Island, NY-NJ-PA MSA (1)
+            *   Buffalo-Niagara Falls, NY MSA (50)
+        *   Pennsylvania
+            *   Philadelphia-Camden-Wilmington, PA-NJ-DE-MD MSA (5)
+            *   Pittsburgh, PA MSA (22)
+        *   Rhode Island
+            *   Providence-New Bedford-Fall River, RI-MA MSA (37)
+    *   Midwest
+        *   Illinois
+            *   Chicago-Joliet-Naperville, IL-IN-WI MSA (3)
+        *   Indiana
+            *   Indianapolis-Carmel, IN MSA (34)
+        *   Michigan
+            *   Detroit-Warren-Livonia, MI MSA (11)
+        *   Minnesota
+            *   Minneapolis-St. Paul-Bloomington, MN-WI MSA (16)
+        *   Missouri
+            *   St. Louis, MO-IL MSA (18)
+            *   Kansas City, MO-KS MSA (29)
+        *   Ohio
+            *   Cincinnati-Middletown, OH-KY-IN MSA (24)
+            *   Cleveland-Elyria-Mentor, OH MSA (26)
+            *   Columbus, OH MSA (32)
+        *   Wisconsin
+            *   Milwaukee-Waukesha-West Allis, WI MSA (39)
+    *   South
+        *   Alabama
+            *   Birmingham-Hoover, AL MSA (47)
+        *   District of Columbia
+            *   Washington-Arlington-Alexandria, DC-VA-MD-WV MSA (8)
+        *   Florida
+            *   Miami-Fort Lauderdale-Pompano Beach, FL MSA (7)
+            *   Tampa-St. Petersburg-Clearwater, FL MSA (19)
+            *   Orlando-Kissimmee-Sanford, FL MSA (27)
+            *   Jacksonville, FL MSA (40)
+        *   Georgia
+            *   Atlanta-Sandy Springs-Marietta, GA MSA (9)
+        *   Kentucky
+            *   Louisville/Jefferson County, KY-IN MSA (42)
+        *   Louisiana
+            *   New Orleans-Metairie-Kenner, LA MSA (46)
+        *   Maryland
+            *   Baltimore-Towson, MD MSA (20)
+        *   North Carolina
+            *   Charlotte-Gastonia-Rock Hill, NC-SC MSA (33)
+            *   Raleigh-Cary, NC MSA (49)
+        *   Oklahoma
+            *   Oklahoma City, OK MSA (44)
+        *   Tennessee
+            *   Nashville-Davidson–Murfreesboro–Franklin, TN MSA (38)
+            *   Memphis, TN-MS-AR MSA (41)
+        *   Texas
+            *   Dallas-Fort Worth-Arlington, TX MSA (4)
+            *   Houston-Sugar Land-Baytown, TX MSA (6)
+            *   San Antonio-New Braunfels, TX MSA (28)
+            *   Austin-Round Rock-San Marcos, TX MSA (35)
+        *   Virginia
+            *   Virginia Beach-Norfolk-Newport News, VA-NC MSA (36)
+            *   Richmond, VA MSA (43)
+    *   West
+        *   Arizona
+            *   Phoenix-Mesa-Glendale, AZ MSA (12)
+        *   California
+            *   Los Angeles-Long Beach-Santa Ana, CA MSA (2)
+            *   San Francisco-Oakland-Fremont, CA MSA (13)
+            *   Riverside-San Bernardino-Ontario, CA MSA (14)
+            *   San Diego-Carlsbad-San Marcos, CA MSA (17)
+            *   Sacramento–Arden-Arcade–Roseville, CA MSA (25)
+            *   San Jose-Sunnyvale-Santa Clara, CA MSA (31)
+        *   Colorado
+            *   Denver-Aurora-Broomfield, CO MSA (21)
+        *   Nevada
+            *   Las Vegas-Paradise, NV MSA (30)
+        *   Oregon
+            *   Portland-Vancouver-Hillsboro, OR-WA MSA (23)
+        *   Utah
+            *   Salt Lake City, UT MSA (48)
+        *   Washington
+            *   Seattle-Tacoma-Bellevue, WA MSA (15)
 
 ## In the wild
 
