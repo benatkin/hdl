51cb9f8fee2ca18b2cb08cc387b14f2ac189f569
Ben Atkin <ben@benatkin.com>
2011-01-17 21:30:13 -0700

    start writing page; extract data from wikipedia

---
+++ msa.py
@@ -0,0 +1,74 @@
+import httplib2
+import os.path
+import lxml.etree
+import lxml.html
+import lxml.cssselect
+import re
+
+class WikiPage(object):
+    def get_xml_url(self):
+        self.xml_url = 'http://en.wikipedia.org/w/api.php?action=parse&prop=text&page=%s&format=xml' % (self.title,)
+
+    def get_xml_filename(self):
+        self.xml_filename = self.title + '.xml'
+
+    def download_xml(self):
+        self.get_xml_url()
+        h = httplib2.Http()
+        headers = {"Accept": "application/xml"}
+        resp, xml = h.request(self.xml_url, headers=headers)
+        with open(self.xml_filename, 'w') as f:
+            f.write(xml)
+
+    def get_xml(self):
+        self.get_xml_filename()
+        if not os.path.exists(self.xml_filename):
+            self.download_xml()
+        with open(self.xml_filename) as f:
+            self.xml = f.read()
+
+    def get_xml_tree(self):
+        self.get_xml()
+        utf8_parser = lxml.etree.XMLParser(encoding='utf-8')
+        self.xml_tree = lxml.etree.fromstring(self.xml, parser=utf8_parser)
+
+    def get_html(self):
+        self.get_xml_tree()
+        self.html = self.xml_tree.xpath('//text')[0].text
+
+    def get_html_tree(self):
+        self.get_html()
+        utf8_parser = lxml.html.HTMLParser(encoding='utf-8')
+        self.html_tree = lxml.html.fromstring(self.html, parser=utf8_parser)
+
+class MSA(WikiPage):
+    title = 'Table_of_United_States_Metropolitan_Statistical_Areas'
+    count = 50
+
+    def get_table(self):
+        self.get_html_tree()
+        sortable_select = lxml.cssselect.CSSSelector('table.sortable')
+        self.table = sortable_select(self.html_tree)[0]
+
+    def get_all_data(self):
+        self.get_table()
+        self.all_data = []
+        for tr in self.table.xpath('.//tr'):
+            td = tr.xpath('.//td')
+            if len(td) > 2:
+                area = {}
+                area['rank'] = int(''.join(td[0].xpath('text()')))
+                area['name'] = ''.join(td[1].xpath('a/text()'))
+                self.all_data.append(area)
+
+    def get_data(self):
+        self.get_all_data()
+        self.data = self.all_data[:self.count]
+                
+    def build(self):
+        self.get_data()
+
+if __name__ == "__main__":
+    msa = MSA()
+    msa.build()
+    print msa.data
---
+++ readme.markdown
@@ -0,0 +1,32 @@
+# Introduction
+
+*Hoisting* and *Deep Linking* are a couple of related software terms I've
+encountered recently. They both help large documents behave like smaller
+documents.
+
+# Hoisting
+
+I learned about hoisting about a year ago when I encountered outliners.com in
+Dave Winer's sidebar. Hoisting is taking one node of an outline and making it
+the root node of a view. It dates back until the 80s, and still shows up
+today, usually under different names.
+
+## City Example
+
+As an example, I'll use an unordered list, of the [20 largest metropolitain
+areas in the U. S., by
+state](http://en.wikipedia.org/wiki/Table_of_United_States_Metropolitan_Statistical_Areas).
+Some of these metro areas are spread across states; I'll use the state which
+has most of the metro's people living in it, which is conveniently listed
+first. To add an additional level, I've broken these into regions.
+
+
+## In the wild
+
+### Two Levels
+
+### Beyond
+
+# Deep Linking
+
+
